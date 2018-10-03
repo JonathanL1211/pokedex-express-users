7c9d82b01c4812c6482ea35b7fa8176891aedfcc
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-10-03 18:48:31 +0800

    add: user and association table

--- tables.sql
+++ tables.sql
@@ -5,3 +5,14 @@ CREATE TABLE IF NOT EXISTS pokemon (
     weight TEXT,
     height TEXT
 );
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS user_pokemon (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    pokemon_id INTEGER
+);
